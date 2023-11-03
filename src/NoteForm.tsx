6732b854ccc235329cf3e5285cca11a903a2a78e
mikel-js <michaelcastro.js@gmail.com>
2023-11-03 23:38:03 +0200

    add function buttons

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Form, Row, Stack } from 'react-bootstrap';
+import { Button, Col, Form, Row, Stack } from 'react-bootstrap';
 import CreatableReactSelect from 'react-select/creatable';
 
 const NoteForm = () => {
@@ -24,6 +24,14 @@ const NoteForm = () => {
           <Form.Label>Body</Form.Label>
           <Form.Control required as='textarea' rows={15}></Form.Control>
         </Form.Group>
+        <Stack direction='horizontal' gap={2} className='justify-content-end'>
+          <Button type='submit' variant='outline-primary'>
+            Save
+          </Button>
+          <Button type='button' variant='outline-secondary'>
+            Cancel
+          </Button>
+        </Stack>
       </Stack>
     </Form>
   );

443a316e477ebaf4e80ab713cba6b2d3ba55b927
mikel-js <michaelcastro.js@gmail.com>
2023-11-03 23:35:43 +0200

    add reat select

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Form, Row, Stack } from 'react-bootstrap';
+import CreatableReactSelect from 'react-select/creatable';
 
 const NoteForm = () => {
   return (
@@ -12,7 +13,17 @@ const NoteForm = () => {
               <Form.Control required></Form.Control>
             </Form.Group>
           </Col>
+          <Col>
+            <Form.Group controlId='tags'>
+              <Form.Label>Tags</Form.Label>
+              <CreatableReactSelect isMulti />
+            </Form.Group>
+          </Col>
         </Row>
+        <Form.Group controlId='markdown'>
+          <Form.Label>Body</Form.Label>
+          <Form.Control required as='textarea' rows={15}></Form.Control>
+        </Form.Group>
       </Stack>
     </Form>
   );

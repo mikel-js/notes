0ea9fc0d2b5170e5f05f3f58252875211191ca85
mikel-js <michaelcastro.js@gmail.com>
2023-11-05 00:07:17 +0200

    fix console error

--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 const useLocalStorage = <T>(key: string, initialValue: T | (() => T)) => {
-  const { value, setValue } = useState<T>(() => {
+  const [value, setValue] = useState<T>(() => {
     const jsonValue = localStorage.getItem(key);
     if (jsonValue == null) {
       if (typeof initialValue === 'function') {

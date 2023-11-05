de2cd60db7fd637f5d889a9c17312fb0e906c89a
mikel-js <michaelcastro.js@gmail.com>
2023-11-05 22:40:11 +0200

    add onsubmit on notes creation

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import NoteForm from './NoteForm';
+import { NoteData } from './App';
 
-const NewNote = () => {
+type NewNoteProps = {
+  onSubmit: (data: NoteData) => void;
+};
+
+const NewNote = ({ onSubmit }: NewNoteProps) => {
   return (
     <div>
       <h1 className='mb-4'>New note</h1>
-      <NoteForm />
+      <NoteForm onSubmit={onSubmit} />
     </div>
   );
 };

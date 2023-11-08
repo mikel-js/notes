3efc41b7d4a816d35d400622f0ab98da1c5638fa
mikel-js <michaelcastro.js@gmail.com>
2023-11-08 22:01:13 +0200

    tags selection added

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,5 +1,6 @@
 import { Button, Col, Form, Row, Stack } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import ReactSelect from 'react-select';
 
 export const NoteList = () => {
   return (
@@ -23,6 +24,27 @@ export const NoteList = () => {
             <Form.Group controlId='title'>
               <Form.Label>Title</Form.Label>
               <Form.Control type='text' />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId='tags'>
+              <Form.Label>Tags</Form.Label>
+              <ReactSelect
+                isMulti
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+              />
             </Form.Group>
           </Col>
         </Row>

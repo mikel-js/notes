b50fe2638c0f7c0c63706dd506ed6284f24da84c
mikel-js <michaelcastro.js@gmail.com>
2023-11-08 21:55:33 +0200

    add title input

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Row, Stack } from 'react-bootstrap';
+import { Button, Col, Form, Row, Stack } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export const NoteList = () => {
@@ -13,9 +13,20 @@ export const NoteList = () => {
             <Link to='/new'>
               <Button variant='primary'>Create</Button>
             </Link>
+            <Button variant='outline-secondary'>Edit Tags</Button>
           </Stack>
         </Col>
       </Row>
+      <Form>
+        <Row className='mb-4'>
+          <Col>
+            <Form.Group controlId='title'>
+              <Form.Label>Title</Form.Label>
+              <Form.Control type='text' />
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
     </>
   );
 };
